a2822558cdb442b1e20700cca02a205c9ba13902
Francis <davidfjc78@gmail.com>
2025-04-29 14:45:41 -0500

    TERCER COMMMIT

--- main.c
+++ main.c
@@ -2,104 +2,96 @@
 #include <string.h>
 
 int main() {
-    int EQUIPOS    = 4;
-    int MAX_NOMBRE = 30;
+    char equipos[4][30];
+    int golesFavor[4] = {0};
+    int golesContra[4] = {0};
+    int diferencia[4] = {0};
+    int puntos[4] = {0};
+    int indices[4];
+    int i, j;
 
-    char nombres[EQUIPOS][MAX_NOMBRE];
-    int  golesFavor[EQUIPOS];
-    int  golesContra[EQUIPOS];
-    int  diferencia[EQUIPOS];
-    int  i, valor, valido;
-
-  
-    for (i = 0; i < EQUIPOS; i++) {
-        do {
-            valido = 1;
-            printf("Ingrese nombre del equipo %d: ", i + 1);
-            fflush(stdin);
-           
-            if (scanf("%29[^\n]", nombres[i]) != 1) {
-                printf("  Nombre invalido. No puede estar vacio.\n");
-                valido = 0;
-            }
-            fflush(stdin);
-            if (valido && strlen(nombres[i]) == 0) {
-                printf("  Nombre invalido. No puede estar vacio.\n");
-                valido = 0;
-            }
-        } while (!valido);
+    // Lectura de nombres de los equipos
+    for (i = 0; i < 4; i++) {
+        printf("Introduce nombre del equipo %d: ", i + 1);
+        scanf("%29s", equipos[i]);
+        fflush(stdin);  // limpia buffer de entrada
     }
 
- 
-    for (i = 0; i < EQUIPOS; i++) {
-     
-        do {
-            valido = 1;
-            printf("Ingrese goles a favor de %s: ", nombres[i]);
+    // Calculo de resultados de los 6 partidos
+    for (i = 0; i < 3; i++) {
+        for (j = i + 1; j < 4; j++) {
+            int g1, g2;
+            // Goles equipo i
+            do {
+                printf("\nPartido: %s vs %s\n", equipos[i], equipos[j]);
+                printf("  Goles de %s: ", equipos[i]);
+                if (scanf("%d", &g1) != 1 || g1 < 0) {
+                    printf("    Error: introduce un numero valido >= 0.\n");
+                    fflush(stdin);
+                } else break;
+            } while (1);
             fflush(stdin);
-            if (scanf("%d", &valor) != 1) {
-                printf("  Entrada invalida. Debe ser un numero entero.\n");
-                valido = 0;
-            } else if (valor < 0) {
-                printf("  Numero invalido. No puede ser negativo.\n");
-                valido = 0;
+            // Goles equipo j
+            do {
+                printf("  Goles de %s: ", equipos[j]);
+                if (scanf("%d", &g2) != 1 || g2 < 0) {
+                    printf("    Error: introduce un numero valido >= 0.\n");
+                    fflush(stdin);
+                } else break;
+            } while (1);
+            fflush(stdin);
+
+            // Actualiza goles a favor y en contra
+            golesFavor[i] += g1;
+            golesContra[i] += g2;
+            golesFavor[j] += g2;
+            golesContra[j] += g1;
+
+            // Asigna puntos segun resultado
+            if (g1 > g2) {
+                puntos[i] += 3;
+            } else if (g2 > g1) {
+                puntos[j] += 3;
+            } else {
+                puntos[i] += 1;
+                puntos[j] += 1;
             }
-            fflush(stdin);
-        } while (!valido);
-        golesFavor[i] = valor;
-
-       
-        do {
-            valido = 1;
-            printf("Ingrese goles en contra de %s: ", nombres[i]);
-            fflush(stdin);
-            if (scanf("%d", &valor) != 1) {
-                printf("  Entrada invalida. Debe ser un numero entero.\n");
-                valido = 0;
-            } else if (valor < 0) {
-                printf("  Numero invalido. No puede ser negativo.\n");
-                valido = 0;
-            }
-            fflush(stdin);
-        } while (!valido);
-        golesContra[i] = valor;
-    }
-
-    
-    for (i = 0; i < EQUIPOS; i++) {
-        diferencia[i] = golesFavor[i] - golesContra[i];
-    }
-
-   
-    int primero = 0, segundo = 1;
-    if (diferencia[1] > diferencia[0]) {
-        primero = 1; segundo = 0;
-    }
-    for (i = 2; i < EQUIPOS; i++) {
-        if (diferencia[i] > diferencia[primero]) {
-            segundo = primero;
-            primero = i;
-        } else if (diferencia[i] > diferencia[segundo]) {
-            segundo = i;
         }
     }
 
-   
-    printf("\n+------------------------------+-------+-------+-------------------+\n");
-    printf("| Equipo                       |  GF   |  GC   | Dif. de goles     |\n");
-    printf("+------------------------------+-------+-------+-------------------+\n");
-    for (i = 0; i < EQUIPOS; i++) {
-        printf("| %-28s | %5d | %5d | %17d |\n",
-               nombres[i], golesFavor[i], golesContra[i], diferencia[i]);
+    // Calcula diferencia de goles e inicializa indices
+    for (i = 0; i < 4; i++) {
+        diferencia[i] = golesFavor[i] - golesContra[i];
+        indices[i] = i;
     }
-    printf("+------------------------------+-------+-------+-------------------+\n");
 
-    
-    printf("\nFinalistas del cuadrangular:\n");
-    printf("  1er lugar: %s (Dif. de goles = %d)\n",
-           nombres[primero], diferencia[primero]);
-    printf("  2do lugar: %s (Dif. de goles = %d)\n",
-           nombres[segundo], diferencia[segundo]);
+    // Ordena equipos por puntos y diferencia de goles (metodo burbuja)
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3 - i; j++) {
+            int a = indices[j];
+            int b = indices[j + 1];
+            if (puntos[a] < puntos[b] ||
+                (puntos[a] == puntos[b] && diferencia[a] < diferencia[b])) {
+                int temp = indices[j];
+                indices[j] = indices[j + 1];
+                indices[j + 1] = temp;
+            }
+        }
+    }
+
+    // Imprime tabla de posiciones
+    printf("\nTabla de posiciones:\n");
+    printf("Equipo\t\tPuntos\tGF\tGC\tGD\n");
+    for (i = 0; i < 4; i++) {
+        int idx = indices[i];
+        printf("%-10s\t%3d\t%3d\t%3d\t%3d\n", equipos[idx], puntos[idx], golesFavor[idx], golesContra[idx], diferencia[idx]);
+    }
+
+    // Finalistas
+    printf("\nFinalistas:\n");
+    printf("1. %s\n", equipos[indices[0]]);
+    printf("2. %s\n", equipos[indices[1]]);
 
     return 0;
 }
+
